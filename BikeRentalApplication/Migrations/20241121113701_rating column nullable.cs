7cca90a4ff234b862f96452353a5af65eef70e65
Paramananthan T. <UT01373@unicomtic.com>
2024-11-21 19:03:12 +0530

    user update pending

---
+++ BikeRentalApplication/Migrations/20241121113701_rating column nullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BikeRentalApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class ratingcolumnnullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Review",
+                table: "RentalRecords",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Review",
+                table: "RentalRecords",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
